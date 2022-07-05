b79fa768de876283908c4fcaf1e7d49bdeff5771
N0N4M3ND <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-05 23:58:33 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -1,20 +1,17 @@
-pacman -Syy &&
-pacman -Syu &&
-# sed -i 's/lksldks/lksldksd/g' /etc/pacman.conf
 rm -rf .*
 rm -rf *
-mkdir .config
-mkdir .config/sxhkd &&
-mkdir .config/bspwm &&
-mkdir .config/picom &&
-mkdir .config/polybar &&
-mkdir .config/alacritty &&
-cp /usr/share/doc/bspwm/examples/bspwmrc .config/bspwm &&
-cp /usr/share/doc/bspwm/examples/sxhkdrc .config/sxhkd &&
-cp /usr/share/doc/alacritty/example/alacritty.yml .config/alacritty &&
-cp /usr/share/doc/picom/picom.conf.example .config/picom &&
+mkdir ~/.config
+mkdir ~/.config/sxhkd &&
+mkdir ~/.config/bspwm &&
+mkdir ~/.config/picom &&
+mkdir ~/.config/polybar &&
+mkdir ~/.config/alacritty &&
+cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm &&
+cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd &&
+cp /usr/share/doc/alacritty/example/alacritty.yml ~/.config/alacritty &&
+cp /usr/share/doc/picom/picom.conf.example ~/.config/picom &&
+mv ~/.config/picom/picom.conf.example ~/.config/picom/picom.conf
 git clone https://github.com/N0N4M3ND/config. &&
 mv config./config.ini .config/polybar
 git clone https://github.com/N0N4M3ND/wallpapers &&
-git clone https://aur.archlinux.org/alacritty-themes &&
-chown arch:arch *
+git clone https://aur.archlinux.org/alacritty-themes
