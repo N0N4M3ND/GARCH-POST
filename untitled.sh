a9cd4d858f6649d76bdeeb6219ba046bf46e78f1
N0N4M3ND <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-05 22:57:39 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -9,10 +9,10 @@ mkdir .config/bspwm &&
 mkdir .config/picom &&
 mkdir .config/polybar &&
 mkdir .config/alacritty &&
-cp /usr/share/docs/bspwm/examples/bspwmrc .config/bspwm &&
-cp /usr/share/docs/bspwm/examples/sxhkdrc .config/sxhkd &&
-cp /usr/share/docs/alacritty/examples/alacritty.yml .config/alacritty &&
-cp /usr/share/docs/picom/picom.conf.examples .config/picom &&
+cp /usr/share/doc/bspwm/examples/bspwmrc .config/bspwm &&
+cp /usr/share/doc/bspwm/examples/sxhkdrc .config/sxhkd &&
+cp /usr/share/doc/alacritty/examples/alacritty.yml .config/alacritty &&
+cp /usr/share/doc/picom/picom.conf.examples .config/picom &&
 git clone https://github.com/N0N4M3ND/config. &&
 mv config./config.ini .config/polybar
 git clone https://github.com/N0N4M3ND/wallpapers &&
