26f4f76dbd278876a005b0d5c5d892b62441d536
N0N4M3ND <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-05 22:59:20 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -11,7 +11,7 @@ mkdir .config/polybar &&
 mkdir .config/alacritty &&
 cp /usr/share/doc/bspwm/examples/bspwmrc .config/bspwm &&
 cp /usr/share/doc/bspwm/examples/sxhkdrc .config/sxhkd &&
-cp /usr/share/doc/alacritty/examples/alacritty.yml .config/alacritty &&
+cp /usr/share/doc/alacritty/example/alacritty.yml .config/alacritty &&
 cp /usr/share/doc/picom/picom.conf.examples .config/picom &&
 git clone https://github.com/N0N4M3ND/config. &&
 mv config./config.ini .config/polybar
