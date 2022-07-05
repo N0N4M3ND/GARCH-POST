ab33e7db1398f4e71f80337250dbe93d4a069b4b
N0N4M3ND <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-05 21:25:10 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -1,1 +1,18 @@
-test
+pacman -Syy &&
+pacman -Syu &&
+mkdir .config/sxhkd &&
+mkdir .config/bspwm &&
+mkdir .config/picom &&
+mkdir .config/polybar &&
+mkdir .config/alacritty &&
+cp /usr/share/docs/bspwm/examples/bspwmrc .config/bspwm &&
+cp /usr/share/docs/bspwm/examples/sxhkdrc .config/sxhkd &&
+cp /usr/share/docs/alacritty/examples/alacritty.yml .config/alacritty &&
+cp /usr/share/docs/picom/picom.conf.examples .config/picom &&
+git clone https://github.com/N0N4M3ND/config. &&
+git clone https://github.com/N0N4M3ND/.wallpapers &&
+git clone https://aur.archlinux.org/alacritty-themes &&
+git clone https://aur.archlinux.org/grapejuice &&
+cd alacritty-themes &&
+makepkg -si
+
