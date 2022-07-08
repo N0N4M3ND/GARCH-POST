64039c2eaba772dffc9b13731240129be861ba60
N0N4M3ND <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-09 00:44:27 +0400

    Add files via upload

---
+++ untitled.sh
@@ -0,0 +1,14 @@
+rm -rf .* &&
+rm -rf * &&
+mkdir ~/.config &&
+mkdir ~/.config/sxhkd &&
+mkdir ~/.config/bspwm &&
+mkdir ~/.config/picom &&
+mkdir ~/.config/polybar &&
+mkdir ~/.config/alacritty &&
+git clone https://github.com/N0N4M3ND/GARCH-POST &&
+mv .config ~/ &&
+chmod +x ~/.config/bspwm/bspwmrc &&
+git clone https://github.com/N0N4M3ND/wallpapers &&
+sudo mv wallpapers /usr/share/backgrounds &&
+reboot
