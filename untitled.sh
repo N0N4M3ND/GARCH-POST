0a4bcfcf459c3d8b6a917e1d087aea6ee0351414
the green man <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-09 01:37:22 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -1,11 +1,5 @@
 rm -rf .* &&
 rm -rf * &&
-mkdir ~/.config &&
-mkdir ~/.config/sxhkd &&
-mkdir ~/.config/bspwm &&
-mkdir ~/.config/picom &&
-mkdir ~/.config/polybar &&
-mkdir ~/.config/alacritty &&
 git clone https://github.com/N0N4M3ND/dotfiles &&
 mv .config ~/ &&
 chmod +x ~/.config/bspwm/bspwmrc &&
