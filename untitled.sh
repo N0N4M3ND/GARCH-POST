4534ac25eff8a82c6581f2da8755d414dc35f0e0
the green man <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-09 16:49:38 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/N0N4M3ND/dotfiles &&
-mv .config ~/ &&
+mv dotfiles/.config ~/ &&
 chmod +x ~/.config/bspwm/bspwmrc &&
 git clone https://github.com/N0N4M3ND/wallpapers &&
 sudo mv wallpapers /usr/share/backgrounds
