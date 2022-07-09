858e865a118c9043977052e1052b06bf6c384d49
the green man <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-10 00:00:18 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -3,3 +3,4 @@ mv dotfiles/.config ~/ &&
 chmod +x ~/.config/bspwm/bspwmrc &&
 git clone https://github.com/N0N4M3ND/wallpapers &&
 sudo mv wallpapers /usr/share/backgrounds
+git clone https://aur.archlinux.org/alacritty-themes
