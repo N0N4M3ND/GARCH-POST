a62b3004624e8e5ebba4c254b54adbd59bc468be
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-19 14:50:34 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -3,8 +3,12 @@ mv dotfiles/.config ~/ &&
 chmod +x .config/bspwm/bspwmrc &&
 git clone https://github.com/N0N4M3ND/wallpapers &&
 git clone https://aur.archlinux.org/cava &&
+git clone https://aur.archlinux.org/ytop &&
 sudo mv wallpapers /usr/share/backgrounds &&
 cd cava &&
+makepkg -si &&
+cd &&
+cd ytop &&
 makepkg -si &&
 cd &&
 rm -rf *
