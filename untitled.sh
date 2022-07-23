9c6cb1886e6922a952cc056fd76fdd47a3e8d89e
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-24 00:23:41 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -2,9 +2,14 @@ git clone https://github.com/N0N4M3ND/dotfiles &&
 mv dotfiles/.config ~/ &&
 chmod +x .config/bspwm/bspwmrc &&
 git clone https://github.com/N0N4M3ND/wallpapers &&
-git clone https://aur.archlinux.org/cava &&
 sudo mv wallpapers /usr/share/backgrounds &&
+git clone https://aur.archlinux.org/cava &&
 cd cava &&
 makepkg -si &&
 cd .. &&
+git clone https://aur.archlinux.org/lightdm-webkit2-theme-glorious &&
+cd lightdm-webkit2-theme-glorious &&
+makepkg -si &&
+sudo sed -i 's/debug_mode          = false/debug_mode          = true/g' /etc/lightdm/lightdm-webkit2-greeter.conf &&
+sudo sed -i 's/webkit_theme          = antergos/webkit_theme          = glorious/g' /etc/lightdm/lightdm-webkit2-greeter.conf &&
 rm -rf *
