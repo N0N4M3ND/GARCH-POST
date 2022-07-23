3b7a276471fd046dd7ced40ed2f1f425ca7ad1a8
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-24 01:40:54 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -13,5 +13,5 @@ makepkg -si &&
 sudo sed -i 's/debug_mode          = false/debug_mode          = true/g' /etc/lightdm/lightdm-webkit2-greeter.conf &&
 sudo sed -i 's/detect_theme_errors = true/detect_theme_errors = false/g' /etc/lightdm/lightdm-webkit2-greeter.conf &&
 sudo sed -i 's/webkit_theme          = antergos/webkit_theme          = glorious/g' /etc/lightdm/lightdm-webkit2-greeter.conf &&
-cd ..
+cd .. &&
 rm -rf *
